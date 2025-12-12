171b117f641694c948a9338d7e31ad1c3e9485ef
GithinjiEdward <152484866+GithinjiEdward@users.noreply.github.com>
2025-12-12 21:57:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function scrollToSection(sectionId) {
+    const target = document.getElementById(sectionId);
+    if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
    
     // ------------------------------------------------------------------
